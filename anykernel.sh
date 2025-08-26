12c288d8f30548fa4b35c1cdef3018d6b5c7fc46
peternmuller <pm563838@gmail.com>
2025-08-26 16:48:51 +0200

    anykernel: alioth -> apollo
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -10,8 +10,8 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=alioth
-device.name2=aliothin
+device.name1=apollo
+device.name2=apollon
 device.name3=
 device.name4=
 device.name5=
@@ -22,7 +22,7 @@ supported.vendorpatchlevels=
 
 # shell variables
 BLOCK=/dev/block/bootdevice/by-name/boot;
-IS_SLOT_DEVICE=1;
+IS_SLOT_DEVICE=0;
 RAMDISK_COMPRESSION=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
@@ -31,6 +31,7 @@ RAMDISK_COMPRESSION=auto;
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 750 750 $RAMDISK/*;
+set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 
 ## AnyKernel install
 dump_boot;
@@ -44,18 +45,3 @@ fi;
 
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end install
-
-# vendor_boot shell variables
-BLOCK=/dev/block/bootdevice/by-name/vendor_boot;
-IS_SLOT_DEVICE=1;
-RAMDISK_COMPRESSION=auto;
-PATCH_VBMETA_FLAG=auto;
-
-# reset for vendor_boot patching
-reset_ak;
-
-# vendor_boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-## end vendor_boot install
